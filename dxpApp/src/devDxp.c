112addb6fc672538a4351fd7fad9725976cb030c
Mark Rivers <MarkRivers@users.noreply.github.com>
2004-12-11 15:48:31 +0000

    Major rewrite for Handel; only use dxpDriver for port thread, not interfaces

--- dxpApp/src/devDxp.c
+++ dxpApp/src/devDxp.c
@@ -18,12 +18,14 @@
 #include <link.h>
 #include <errlog.h>
 #include <dbCommon.h>
+#include <dbEvent.h>
 #include <recSup.h>
 #include <recGbl.h>
 #include <alarm.h>
 #include <cantProceed.h>
 #include <epicsExport.h>
 #include <epicsString.h>
+#include <epicsThread.h>
 
 #include <asynDriver.h>
 #include <asynInt32.h>
@@ -31,56 +33,36 @@
 
 #include "dxpRecord.h"
 #include "devDxp.h"
-#include "asynDxp.h"
-#include "mca.h"
-#include "xerxes_structures.h"
-#include "xerxes.h"
+#include "handel.h"
 
 typedef struct {
     dxpRecord *pdxp;
     asynUser *pasynUser;
     char *portName;
     int channel;
-    asynInt32 *pint32;
-    void *asynInt32Pvt;
-    asynDxp *pasynDxp;
-    void *asynDxpPvt;
-    unsigned short nsymbols;
-    unsigned int nbase;
-    unsigned short *params;
-    unsigned short *baseline;
 } devDxpPvt;
 
-typedef enum {
-   mcaMessage,
-   dxpMessage
-} dxpMessage_t;
-
 typedef struct {
-    dxpMessage_t type;
-    mcaCommand mcaCommand;
     devDxpCommand dxpCommand;
-    int value1;
-    int value2;
+    char *name;
+    short param;
+    double dvalue;
+    void *pointer;
 } devDxpMessage;
 
 static long init_record(dxpRecord *pdxp, int *module);
-static long send_mca_msg(dxpRecord *pdxp, mcaCommand command); 
 static long send_dxp_msg(dxpRecord *pdxp, devDxpCommand command, 
-                         int v1, int v2);
-static long send_msg(dxpRecord *pdxp, devDxpMessage *msg); 
-static long read_array(dxpRecord *pdxp);
+                         char *name, int param, double dvalue,
+                         void *pointer);
 static void asynCallback(asynUser *pasynUser);
 
 static const struct devDxpDset devDxp = {
-    7,
+    5,
     NULL,
     NULL,
     init_record,
     NULL,
-    send_mca_msg,
     send_dxp_msg,
-    read_array
 };
 epicsExportAddress(dset, devDxp);
 
@@ -90,7 +72,6 @@ static long init_record(dxpRecord *pdxp, int *module)
     char *userParam;
     asynUser *pasynUser;
     asynStatus status;
-    asynInterface *pasynInterface;
     devDxpPvt *pPvt;
 
     /* Allocate asynDxpPvt private structure */
@@ -122,32 +103,6 @@ static long init_record(dxpRecord *pdxp, int *module)
         goto bad;
     }
 
-    /* Get the asynDxp interface */
-    pasynInterface = pasynManager->findInterface(pasynUser, asynDxpType, 1);
-    if (!pasynInterface) {
-        asynPrint(pasynUser, ASYN_TRACE_ERROR,
-                  "devDxp::init_record, find dxp interface failed\n");
-        goto bad;
-    }
-    pPvt->pasynDxp = (asynDxp *)pasynInterface->pinterface;
-    pPvt->asynDxpPvt = pasynInterface->drvPvt;
-
-    /* Get the asynInt32 interface */
-    pasynInterface = pasynManager->findInterface(pasynUser, asynInt32Type, 1);
-    if (!pasynInterface) {
-        asynPrint(pasynUser, ASYN_TRACE_ERROR,
-                  "devDxp::init_record, find int32 interface failed\n");
-        goto bad;
-    }
-    pPvt->pint32 = (asynInt32 *)pasynInterface->pinterface;
-    pPvt->asynInt32Pvt = pasynInterface->drvPvt;
-
-    dxp_max_symbols(&pPvt->channel, &pPvt->nsymbols);
-    dxp_nbase(&pPvt->channel, &pPvt->nbase);
-    pPvt->params = callocMustSucceed(pPvt->nsymbols, sizeof(unsigned short),
-                                     "devMcaAsyn init_record()");
-    pPvt->baseline = callocMustSucceed(pPvt->nbase, sizeof(unsigned short),
-                                     "devMcaAsyn init_record()");
     return(0);
 bad:
     pdxp->pact = 1;
@@ -155,42 +110,30 @@ bad:
 }
 
 
-static long send_mca_msg(dxpRecord *pdxp, mcaCommand command)
-{
-    devDxpMessage *pmsg = pasynManager->memMalloc(sizeof(*pmsg));
-
-    pmsg->type = mcaMessage;
-    pmsg->mcaCommand = command;
-    return(send_msg(pdxp, pmsg));
-}
-
 static long send_dxp_msg(dxpRecord *pdxp, devDxpCommand command, 
-                         int value1, int value2)
-{
-    devDxpMessage *pmsg = pasynManager->memMalloc(sizeof(*pmsg));
-
-    pmsg->type = dxpMessage;
-    pmsg->dxpCommand = command;
-    pmsg->value1 = value1;
-    pmsg->value2 = value2;
-    return(send_msg(pdxp, pmsg));
-}
-
-static long send_msg(dxpRecord *pdxp, devDxpMessage *pmsg)
+                         char *name, int param, double dvalue,
+                         void *pointer)
 {
     devDxpPvt *pPvt = (devDxpPvt *)pdxp->dpvt;
     asynUser *pasynUser;
     int status;
+    devDxpMessage *pmsg = pasynManager->memMalloc(sizeof(*pmsg));
 
-    /* Note that the only message that requires a callback to the record is
-     * send by read_array, and it has already set pact=1 */
+    pmsg->dxpCommand = command;
+    pmsg->name = name;
+    pmsg->param = param;
+    pmsg->dvalue = dvalue;
+    pmsg->pointer = pointer;
 
     asynPrint(pPvt->pasynUser, ASYN_TRACE_FLOW,
-              "devDxp::send_msg: type=%d, command=%d, pact=%d\n",
-              pmsg->type, 
-              (pmsg->type == mcaMessage) ? pmsg->mcaCommand : pmsg->dxpCommand, 
-              pdxp->pact);
+              "devDxp::send_dxp_msg: command=%d, pact=%d"
+              "name=%s, param=%d, dvalue=%f, pointer=%p\n",
+              pmsg->dxpCommand, pdxp->pact,
+              name, param, dvalue, pointer);
 
+    if (pdxp->pact) {
+       return(0);
+    }
     pasynUser = pasynManager->duplicateAsynUser(pPvt->pasynUser,
                                                 asynCallback, 0);
     pasynUser->userData = pmsg;
@@ -198,10 +141,11 @@ static long send_msg(dxpRecord *pdxp, devDxpMessage *pmsg)
     status = pasynManager->queueRequest(pasynUser, 0, 0);
     if (status != asynSuccess) {
         asynPrint(pasynUser, ASYN_TRACE_ERROR,
-                  "devDxp::send_msg: error calling queueRequest, %s\n",
+                  "devDxp::send_dxp_msg: error calling queueRequest, %s\n",
                   pasynUser->errorMessage);
         return(-1);
     }
+    if (interruptAccept) pdxp->pact = 1;
     return(0);
 }
 
@@ -212,82 +156,131 @@ void asynCallback(asynUser *pasynUser)
     devDxpMessage *pmsg = pasynUser->userData;
     dxpRecord *pdxp = pPvt->pdxp;
     rset *prset = (rset *)pdxp->rset;
+    dxpReadbacks *pdxpReadbacks = pdxp->rbptr;
     int status;
+    int detChan;
+    void *pfield;
+    double info[2];
+    double dvalue;
 
+    pasynManager->getAddr(pasynUser, &detChan);
     asynPrint(pasynUser, ASYN_TRACE_FLOW,
-              "devDxp::asynCallback: type=%d, command=%d\n",
-              pmsg->type, 
-              (pmsg->type == mcaMessage) ? pmsg->mcaCommand:pmsg->dxpCommand);
+              "devDxp::asynCallback: command=%d\n",
+              pmsg->dxpCommand);
 
-    switch (pmsg->type) {
-    case mcaMessage:
-        pPvt->pasynUser->reason = pmsg->mcaCommand;
-        pPvt->pint32->write(pPvt->asynInt32Pvt,
-                            pPvt->pasynUser,
-                            0);
-        break;
-    case dxpMessage:
-        switch(pmsg->dxpCommand) {
-        case MSG_DXP_SET_SHORT_PARAM:
-            pPvt->pasynDxp->setShortParam(pPvt->asynDxpPvt,
-                                          pPvt->pasynUser,
-                                          pmsg->value1, pmsg->value2);
-            break;
-        case MSG_DXP_DOWNLOAD_FIPPI:
-            pPvt->pasynDxp->downloadFippi(pPvt->asynDxpPvt,
-                                          pPvt->pasynUser,
-                                          pmsg->value1);
-            break;
-        case MSG_DXP_CONTROL_TASK:
-            pPvt->pasynDxp->controlTask(pPvt->asynDxpPvt,
-                                        pPvt->pasynUser,
-                                        pmsg->value1, pmsg->value2);
-            /* If this is a command that returns data, read it */
-            break;
-        case MSG_DXP_READ_PARAMS:
-            pPvt->pasynDxp->readParams(pPvt->asynDxpPvt,
-                                       pPvt->pasynUser,
-                                       pPvt->params, pPvt->baseline);
-            dbScanLock((dbCommon *)pdxp);
-            (*prset->process)(pdxp);
-            dbScanUnlock((dbCommon *)pdxp);
-            break;
-        default:
-            asynPrint(pasynUser, ASYN_TRACE_ERROR, 
-                      "devDxp::asynCallback, invalid command=%d\n",
-                      pmsg->dxpCommand);
-            break;
-        }
-        break;
-    default:
-        asynPrint(pasynUser, ASYN_TRACE_ERROR, 
-                  "devDxp::asynCallback, invalid message type=%d\n",
-                  pmsg->type);
-    }
+     switch(pmsg->dxpCommand) {
+     case MSG_DXP_SET_SHORT_PARAM:
+         asynPrint(pasynUser, ASYN_TRACEIO_DEVICE,
+             "devDxp::asynCallback, calling xiaSetAcquisitionValues name=%s value=%d\n",
+             pmsg->name, pmsg->param);
+         /* Note that we use xiaSetAcquisitionValues rather than xiaSetParameter
+          * so that the new value will be save with xiaSaveSystem */
+         dvalue = pmsg->param;
+         xiaSetAcquisitionValues(detChan, pmsg->name, &dvalue);
+         break;
+     case MSG_DXP_SET_DOUBLE_PARAM:
+         pfield = pmsg->pointer;
+         if (pfield == (void *)&pdxp->slow_trig) {
+             /* Convert from keV to eV */
+             dvalue = pmsg->dvalue * 1000.;
+             xiaSetAcquisitionValues(detChan, "energy_threshold", &pmsg->dvalue);
+         }
+         else if (pfield == (void *)&pdxp->pktim) {
+             xiaSetAcquisitionValues(detChan, "peaking_time", &pmsg->dvalue);
+         }
+         else if (pfield == (void *)&pdxp->gaptim) {
+             xiaSetAcquisitionValues(detChan, "gap_time", &pmsg->dvalue);
+         }
+         else if (pfield == (void *)&pdxp->adc_rule) {
+             /* Make our "calibration energy be emax/2. in eV */
+             dvalue = pdxp->emax * 1000. / 2.;
+             xiaSetAcquisitionValues(detChan, "calibration_energy", &dvalue);
+             xiaSetAcquisitionValues(detChan, "adc_percent_rule", &pmsg->dvalue);
+         }
+         else if (pfield == (void *)&pdxp->fast_trig) {
+             /* Convert from keV to eV */
+             dvalue = pmsg->dvalue * 1000.;
+             xiaSetAcquisitionValues(detChan, "trigger_threshold", &dvalue);
+         }
+         else if (pfield == (void *)&pdxp->trig_pktim) {
+             xiaSetAcquisitionValues(detChan, "trigger_peaking_time", &pmsg->dvalue);
+         }
+         else if (pfield == (void *)&pdxp->trig_gaptim) {
+             xiaSetAcquisitionValues(detChan, "trigger_gap_time", &pmsg->dvalue);
+         }
+         else if (pfield == (void *)&pdxp->emax) {
+             if (pdxpReadbacks->number_mca_channels <= 0.)
+                 pdxpReadbacks->number_mca_channels = 2048.;
+             /* Set the bin width in eV */
+             dvalue = pmsg->dvalue *1000. / pdxpReadbacks->number_mca_channels;
+             xiaSetAcquisitionValues(detChan, "mca_bin_width", &dvalue);
+         }
+         break;
+     case MSG_DXP_CONTROL_TASK:
+         info[0] = 1.;
+         info[1] = pmsg->dvalue;
+         xiaDoSpecialRun(detChan, pmsg->name, info);
+         if (strcmp(pmsg->name, "adc_trace") == 0) {
+            epicsThreadSleep(0.1);
+            xiaStopRun(detChan);
+            xiaGetSpecialRunData(detChan, "adc_trace", pdxp->tptr);
+            pdxpReadbacks->newAdcTrace = 1;
+         }
+         else if (strcmp(pmsg->name, "baseline_history") == 0) {
+            epicsThreadSleep(0.1);
+            xiaStopRun(detChan);
+            xiaGetSpecialRunData(detChan, "baseline_history", pdxp->bhptr);
+            pdxpReadbacks->newBaselineHistory = 1;
+         }   
+         break;
+     case MSG_DXP_READ_PARAMS:
+         xiaGetRunData(detChan, "baseline", pdxp->bptr);
+         pdxpReadbacks->newBaselineHistogram = 1;
+         xiaGetRunData(detChan, "input_count_rate", &pdxpReadbacks->icr);
+         xiaGetRunData(detChan, "output_count_rate", &pdxpReadbacks->ocr);
+         xiaGetRunData(detChan, "triggers", &pdxpReadbacks->fast_peaks);
+         xiaGetRunData(detChan, "events_in_run", &pdxpReadbacks->slow_peaks);
+         xiaGetParamData(detChan, "values", pdxp->pptr);
+         xiaGetAcquisitionValues(detChan, "energy_threshold", 
+                                 &pdxpReadbacks->slow_trig);
+         xiaGetAcquisitionValues(detChan, "peaking_time", 
+                                 &pdxpReadbacks->pktim);
+         xiaGetAcquisitionValues(detChan, "gap_time", 
+                                 &pdxpReadbacks->gaptim);
+         xiaGetAcquisitionValues(detChan, "adc_percent_rule", 
+                                 &pdxpReadbacks->adc_rule);
+         xiaGetAcquisitionValues(detChan, "trigger_threshold", 
+                                 &pdxpReadbacks->fast_trig);
+         xiaGetAcquisitionValues(detChan, "trigger_peaking_time", 
+                                 &pdxpReadbacks->trig_pktim);
+         xiaGetAcquisitionValues(detChan, "trigger_gap_time", 
+                                 &pdxpReadbacks->trig_gaptim);
+         xiaGetAcquisitionValues(detChan, "mca_bin_width", 
+                                 &pdxpReadbacks->mca_bin_width);
+         xiaGetAcquisitionValues(detChan, "number_mca_channels", 
+                                 &pdxpReadbacks->number_mca_channels);
+         /* Convert count rates from kHz to Hz */
+         pdxpReadbacks->icr *= 1000.;
+         pdxpReadbacks->ocr *= 1000.;
+         /* Convert energy thresholds from eV to keV */
+         pdxpReadbacks->fast_trig /= 1000.;
+         pdxpReadbacks->slow_trig /= 1000.;
+         break;
+     default:
+         asynPrint(pasynUser, ASYN_TRACE_ERROR, 
+                   "devDxp::asynCallback, invalid command=%d\n",
+                   pmsg->dxpCommand);
+         break;
+     }
     pasynManager->memFree(pmsg, sizeof(*pmsg));
     status = pasynManager->freeAsynUser(pasynUser);
     if (status != asynSuccess) {
         asynPrint(pasynUser, ASYN_TRACE_ERROR,
                   "devMcaAsyn::asynCallback: error in freeAsynUser\n");
     }
-}
-
-
-static long read_array(dxpRecord *pdxp)
-{
-    devDxpPvt *pPvt = (devDxpPvt *)pdxp->dpvt;
-
-    /* When this routine is called with pact=0 it sends a message */
-    if (pdxp->pact == 0) {
-       pdxp->pact = 1;
-       send_dxp_msg(pdxp, MSG_DXP_READ_PARAMS, 0, 0);
-       return(0);
-    }
-    /* pact was non-zero, so this is a callback from record after I/O 
-       completes */
-    /* Copy data from private buffer to record */
-    memcpy(pdxp->pptr, pPvt->params, pPvt->nsymbols*sizeof(unsigned short));
-    memcpy(pdxp->bptr, pPvt->baseline, pPvt->nbase*sizeof(unsigned short));
-    if (pdxp->udf==1) pdxp->udf=2;
-    return(0);
+    if (pdxp->udf==1) pdxp->udf=0;
+    if (!interruptAccept) return;
+    dbScanLock((dbCommon *)pdxp);
+    (*prset->process)(pdxp);
+    dbScanUnlock((dbCommon *)pdxp);
 }
