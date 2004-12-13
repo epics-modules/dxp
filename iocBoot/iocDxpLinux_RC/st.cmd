dfdea28fcf19ddad81de53cd0fb4e5df639d73dc
Mark Rivers <MarkRivers@users.noreply.github.com>
2004-12-13 00:58:43 +0000

    New syntax for DXPConfig

--- iocBoot/iocDxpLinux_RC/st.cmd
+++ iocBoot/iocDxpLinux_RC/st.cmd
@@ -15,8 +15,8 @@ xiaSetLogLevel(3)
 xiaInit("test2.ini")
 xiaStartSystem
 
-# DXPConfig(serverName, detChan1, detChan2, detChan3, detChan4, queueSize)
-DXPConfig("DXP1",  0,  -1, -1, -1, 300)
+# DXPConfig(serverName, nchans)
+DXPConfig("DXP1",  1)
 
 dbLoadRecords("$(DXP)/dxpApp/Db/dxp2x_rc.db","P=dxpLinux:, R=dxp1, INP=@asyn(DXP1 0)")
 dbLoadRecords("$(MCA)/mcaApp/Db/mca.db", "P=dxpLinux:, M=mca1, DTYP=asynMCA,INP=@asyn(DXP1 0),NCHAN=2048")
