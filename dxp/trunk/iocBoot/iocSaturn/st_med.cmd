355d36b332b01e59f7ec8f65ccca4569fe86b8dc
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2005-03-10 04:31:30 +0000

    Delete sca_to_roi, done in SNL code now

--- dxp/trunk/iocBoot/iocSaturn/st_med.cmd
+++ dxp/trunk/iocBoot/iocSaturn/st_med.cmd
@@ -29,9 +29,6 @@ dbLoadRecords("../../dxpApp/Db/dxpMED.db","P=dxpSaturn:med:")
 # MCA record
 dbLoadRecords("../../../mca/mcaApp/Db/mca.db", "P=dxpSaturn:med:, M=mca1, DTYP=asynMCA,INP=@asyn(DXP1 0),NCHAN=2048")
 
-# Database to copy MCA ROIs to DXP SCAs
-dbLoadRecords("../../dxp/dxpApp/Db/roi_to_sca.db", "P=dxpSaturn:med:, M=mca1, D=dxp1")
-
 # Setup for save_restore
 < ../save_restore.cmd
 save_restoreSet_status_prefix("dxpSaturn:med:")
