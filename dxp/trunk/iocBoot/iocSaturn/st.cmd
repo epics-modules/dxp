e3e88c1473989ce6843dce31e8016446df6c1b82
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2005-09-22 16:00:15 +0000

    Added 40MHz commands; use absolute rather than relative paths

--- dxp/trunk/iocBoot/iocSaturn/st.cmd
+++ dxp/trunk/iocBoot/iocSaturn/st.cmd
@@ -1,3 +1,5 @@
+< envPaths
+
 # Tell EPICS all about the record types, device-support modules, drivers,
 # etc. in this build from dxpApp
 dbLoadDatabase("../../dbd/dxp.dbd")
@@ -9,9 +11,11 @@ xiaSetLogLevel(2)
 # Execute the following line if you have a Vortex detector or 
 # another detector with a reset pre-amplifier
 xiaInit("vortex.ini")
+#xiaInit("vortex40MHz.ini")
 # Execute the following line if you have a Ketek detector or 
 # another detector with an RC pre-amplifier
 #xiaInit("ketek.ini")
+#xiaInit("ketek40MHz.ini")
 xiaStartSystem
 
 # DXPConfig(serverName, nchans)
@@ -26,7 +30,7 @@ dbLoadRecords("../../dxpApp/Db/dxp2x_reset.db","P=dxpSaturn:, R=dxp1, INP=@asyn(
 #dbLoadRecords("../../dxpApp/Db/dxp2x_rc.db","P=dxpSaturn:, R=dxp1, INP=@asyn(DXP1 0)")
 
 # MCA record
-dbLoadRecords("../../../mca/mcaApp/Db/mca.db", "P=dxpSaturn:, M=mca1, DTYP=asynMCA,INP=@asyn(DXP1 0),NCHAN=2048")
+dbLoadRecords("$(MCA)/mcaApp/Db/mca.db", "P=dxpSaturn:, M=mca1, DTYP=asynMCA,INP=@asyn(DXP1 0),NCHAN=2048")
 
 # Template to copy MCA ROIs to DXP SCAs
 dbLoadTemplate("roi_to_sca.substitutions")
@@ -34,7 +38,7 @@ dbLoadTemplate("roi_to_sca.substitutions")
 # Setup for save_restore
 < ../save_restore.cmd
 save_restoreSet_status_prefix("dxpSaturn:")
-dbLoadRecords("../../../autosave/asApp/Db/save_restoreStatus.db", "P=dxpSaturn:")
+dbLoadRecords("$(AUTOSAVE)/asApp/Db/save_restoreStatus.db", "P=dxpSaturn:")
 set_pass0_restoreFile("auto_settings.sav")
 set_pass1_restoreFile("auto_settings.sav")
 
