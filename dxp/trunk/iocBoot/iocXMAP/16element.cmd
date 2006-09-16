dbafec1429b7b9e1d3972762a0663e40310b578d
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2006-09-16 14:26:09 +0000

    Add scan database; add saveData; change to macro to create_monitor_set

--- dxp/trunk/iocBoot/iocXMAP/16element.cmd
+++ dxp/trunk/iocBoot/iocXMAP/16element.cmd
@@ -14,8 +14,6 @@ set_pass1_restoreFile("auto_settings16.sav")
 
 # Set logging level (1=ERROR, 2=WARNING, 3=XXX, 4=DEBUG)
 xiaSetLogLevel(2)
-# Execute the following line if you have a Vortex detector or
-# another detector with a reset pre-amplifier
 xiaInit("xmap16.ini")
 xiaStartSystem
 
@@ -29,11 +27,19 @@ dbLoadTemplate("16element.template")
 #asynSetTraceIOMask DXP1 0 2
 #var dxpRecordDebug 10
 
+### Scan-support software
+# crate-resident scan.  This executes 1D, 2D, 3D, and 4D scans, and caches
+# 1D data, but it doesn't store anything to disk.  (See 'saveData' below for that.)
+dbLoadRecords("$(SSCAN)/sscanApp/Db/scan.db","P=dxpXMAP:,MAXPTS1=2000,MAXPTS2=1000,MAXPTS3=10,MAXPTS4=10,MAXPTSH=2048")
+
 iocInit
 
-seq dxpMED, "P=dxpXMAP:med:, DXP=dxp, MCA=mca, N_DETECTORS=16"
+seq dxpMED, "P=dxpXMAP:, DXP=dxp, MCA=mca, N_DETECTORS=16"
 
 ### Start up the autosave task and tell it what to do.
 # Save settings every thirty seconds
-create_monitor_set("auto_settings16.req", 30)
+create_monitor_set("auto_settings16.req", 30, "P=dxpXMAP:")
+
+### Start the saveData task.
+saveData_Init("saveData.req", "P=dxpXMAP:")
 
