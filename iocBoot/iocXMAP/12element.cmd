955fb595b7cf9aa83659c0c77e72b563957c66f9
Mark Rivers <MarkRivers@users.noreply.github.com>
2006-09-19 20:22:34 +0000

    Changed prefix; add scans and saveData

--- iocBoot/iocXMAP/12element.cmd
+++ iocBoot/iocXMAP/12element.cmd
@@ -14,8 +14,6 @@ set_pass1_restoreFile("auto_settings12.sav")
 
 # Set logging level (1=ERROR, 2=WARNING, 3=XXX, 4=DEBUG)
 xiaSetLogLevel(2)
-# Execute the following line if you have a Vortex detector or
-# another detector with a reset pre-amplifier
 xiaInit("xmap12.ini")
 xiaStartSystem
 
@@ -25,11 +23,23 @@ DXPConfig("DXP1",  12, 1, 100)
 dbLoadTemplate("12element.template")
 
 #xiaSetLogLevel(4)
+#asynSetTraceMask DXP1 0 255
+#asynSetTraceIOMask DXP1 0 2
+#var dxpRecordDebug 10
+
+### Scan-support software
+# crate-resident scan.  This executes 1D, 2D, 3D, and 4D scans, and caches
+# 1D data, but it doesn't store anything to disk.  (See 'saveData' below for that.)
+dbLoadRecords("$(SSCAN)/sscanApp/Db/scan.db","P=dxpXMAP:,MAXPTS1=2000,MAXPTS2=1000,MAXPTS3=10,MAXPTS4=10,MAXPTSH=2048")
+
 iocInit
 
-seq dxpMED, "P=dxpXMAP:med:, DXP=dxp, MCA=mca, N_DETECTORS=12"
+seq dxpMED, "P=dxpXMAP:, DXP=dxp, MCA=mca, N_DETECTORS=12"
 
 ### Start up the autosave task and tell it what to do.
 # Save settings every thirty seconds
-create_monitor_set("auto_settings12.req", 30)
+create_monitor_set("auto_settings12.req", 30, "P=dxpXMAP:")
+
+### Start the saveData task.
+saveData_Init("saveData.req", "P=dxpXMAP:")
 
--- iocBoot/iocXMAP/4element.cmd
+++ iocBoot/iocXMAP/4element.cmd
@@ -14,8 +14,6 @@ set_pass1_restoreFile("auto_settings4.sav")
 
 # Set logging level (1=ERROR, 2=WARNING, 3=XXX, 4=DEBUG)
 xiaSetLogLevel(2)
-# Execute the following line if you have a Vortex detector or
-# another detector with a reset pre-amplifier
 xiaInit("xmap4.ini")
 xiaStartSystem
 
@@ -25,21 +23,23 @@ DXPConfig("DXP1",  4, 1, 100)
 dbLoadTemplate("4element.template")
 
 #xiaSetLogLevel(4)
-#asynSetTraceMask DXP1   0 255
+#asynSetTraceMask DXP1 0 255
 #asynSetTraceIOMask DXP1 0 2
-#asynSetTraceMask DXP1   1 255
-#asynSetTraceIOMask DXP1 1 2
-#asynSetTraceMask DXP1   2 255
-#asynSetTraceIOMask DXP1 2 2
-#asynSetTraceMask DXP1   3 255
-#asynSetTraceIOMask DXP1 3 2
-#var dxpRecordDebug 20
+#var dxpRecordDebug 10
+
+### Scan-support software
+# crate-resident scan.  This executes 1D, 2D, 3D, and 4D scans, and caches
+# 1D data, but it doesn't store anything to disk.  (See 'saveData' below for that.)
+dbLoadRecords("$(SSCAN)/sscanApp/Db/scan.db","P=dxpXMAP:,MAXPTS1=2000,MAXPTS2=1000,MAXPTS3=10,MAXPTS4=10,MAXPTSH=2048")
 
 iocInit
 
-seq dxpMED, "P=dxpXMAP:med:, DXP=dxp, MCA=mca, N_DETECTORS=4"
+seq dxpMED, "P=dxpXMAP:, DXP=dxp, MCA=mca, N_DETECTORS=4"
 
 ### Start up the autosave task and tell it what to do.
 # Save settings every thirty seconds
-create_monitor_set("auto_settings4.req", 30)
+create_monitor_set("auto_settings4.req", 30, "P=dxpXMAP:")
+
+### Start the saveData task.
+saveData_Init("saveData.req", "P=dxpXMAP:")
 
