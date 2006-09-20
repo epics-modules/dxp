40e3935e623b175b885763fa909204a8d0001ab8
Mark Rivers <MarkRivers@users.noreply.github.com>
2006-09-20 19:47:35 +0000

    Use template file; change prefix; add scans; add envPaths

--- iocBoot/iocSaturn/st_med.cmd
+++ iocBoot/iocSaturn/st_med.cmd
@@ -1,3 +1,5 @@
+< envPaths
+
 # Tell EPICS all about the record types, device-support modules, drivers,
 # etc. in this build from dxpApp
 dbLoadDatabase("../../dbd/dxp.dbd")
@@ -8,33 +10,34 @@ dxp_registerRecordDeviceDriver(pdbbase)
 xiaSetLogLevel(2)
 # Execute the following line if you have a Vortex detector or 
 # another detector with a reset pre-amplifier
-xiaInit("vortex.ini")
+#xiaInit("vortex.ini")
+xiaInit("vortex40MHz.ini")
 # Execute the following line if you have a Ketek detector or 
 # another detector with an RC pre-amplifier
 #xiaInit("ketek.ini")
+#xiaInit("ketek40MHz.ini")
 xiaStartSystem
 
-# DXPConfig(serverName, nchans)
-DXPConfig("DXP1",  1)
+# DXPConfig(serverName, ndetectors, ngroups, pollFrequency)
+DXPConfig("DXP1",  1, 1, 100)
+
 
 # DXP record
 # Execute the following line if you have a Vortex detector or 
 # another detector with a reset pre-amplifier
-dbLoadRecords("../../dxpApp/Db/dxp2x_reset.db","P=dxpSaturn:med:, R=dxp1, INP=@asyn(DXP1 0)")
-dbLoadRecords("../../dxpApp/Db/dxpMED.db","P=dxpSaturn:med:")
-# Execute the following line if you have a Ketek detector or 
-# another detector with an RC pre-amplifier
-#dbLoadRecords("../../dxpApp/Db/dxp2x_rc.db","P=dxpSaturn:med:, R=dxp1, INP=@asyn(DXP1 0)")
-
-# MCA record
-dbLoadRecords("../../../mca/mcaApp/Db/mca.db", "P=dxpSaturn:med:, M=mca1, DTYP=asynMCA,INP=@asyn(DXP1 0),NCHAN=2048")
+dbLoadTemplate("1element.template")
 
 # Setup for save_restore
 < ../save_restore.cmd
-save_restoreSet_status_prefix("dxpSaturn:med:")
-dbLoadRecords("../../../autosave/asApp/Db/save_restoreStatus.db", "P=dxpSaturn:med:")
+save_restoreSet_status_prefix("dxpSaturn:")
+dbLoadRecords("$(AUTOSAVE)/asApp/Db/save_restoreStatus.db", "P=dxpSaturn:")
 set_pass0_restoreFile("auto_settings_med.sav")
 set_pass1_restoreFile("auto_settings_med.sav")
+
+### Scan-support software
+# crate-resident scan.  This executes 1D, 2D, 3D, and 4D scans, and caches
+# 1D data, but it doesn't store anything to disk.  (See 'saveData' below for that.)
+dbLoadRecords("$(SSCAN)/sscanApp/Db/scan.db","P=dxpSaturn:,MAXPTS1=2000,MAXPTS2=1000,MAXPTS3=10,MAXPTS4=10,MAXPTSH=2048")
 
 # Debugging flags
 #asynSetTraceMask DXP1 0 255
@@ -46,7 +49,10 @@ iocInit
 ### Start up the autosave task and tell it what to do.
 
 # Save settings every thirty seconds
-create_monitor_set("auto_settings_med.req", 30)
+create_monitor_set("auto_settings_med.req", 30, P=dxpSaturn:)
+
+### Start the saveData task.
+saveData_Init("saveData.req", "P=dxpSaturn:")
 
 # Start the sequence program for multi-element detector
-seq &dxpMED, "P=dxpSaturn:med:, DXP=dxp, MCA=mca, N_DETECTORS=1"
+seq &dxpMED, "P=dxpSaturn:, DXP=dxp, MCA=mca, N_DETECTORS=1"
