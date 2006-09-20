b09aabbc01358a779e9a917732734a6fd028a6e1
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2006-09-20 03:52:38 +0000

    Comment out EPICS_DISPLAY_PATH definition, too hard to maintain

--- dxp/trunk/iocBoot/iocXMAP/START_IOC.bat
+++ dxp/trunk/iocBoot/iocXMAP/START_IOC.bat
@@ -1,8 +1,8 @@
-REM Tell medm where to look for .adl files.  If this environment variable is already set then
-REM comment out the following line
-set EPICS_DISPLAY_PATH=..\..\dxpApp\op\adl;..\..\..\mca\mcaApp\op\adl;..\..\..\autosave\asApp\op\adl
+REM Tell medm where to look for .adl files.  Edit the following line, or comment it
+REM out if the environment variable is already set
+REM set EPICS_DISPLAY_PATH=C:\epics_adls
 REM Note that we must use the Windows "start" command or medm won't find X11 dlls
-start medm -x -macro "P=dxpXMAP:, D=dxp1, M=mca1" single_dxp_top.adl
+start medm -x -macro "P=dxpXMAP:, D=dxp, M=mca" 16element_dxp.adl
 REM Put Cygwin in the path so the EPICS application can find cygwin1.dll
 PATH=c:\cygwin\bin;C:\XIA_xmap_dlls
 ..\..\bin\cygwin-x86\xmapApp.exe st.cmd
