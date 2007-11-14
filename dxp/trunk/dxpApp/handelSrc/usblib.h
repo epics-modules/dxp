ac2367f61f07f538f24d6bedabee5cb5d3aba5d4
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2007-11-14 21:55:34 +0000

    New version from 4Pi with some of my changes

--- dxp/trunk/dxpApp/handelSrc/usblib.h
+++ dxp/trunk/dxpApp/handelSrc/usblib.h
@@ -1,52 +1,28 @@
-/*
- * Copyright (c) 2004, X-ray Instrumentation Associates
- *               2005, XIA LLC
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, 
- * with or without modification, are permitted provided 
- * that the following conditions are met:
- *
- *   * Redistributions of source code must retain the above 
- *     copyright notice, this list of conditions and the 
- *     following disclaimer.
- *   * Redistributions in binary form must reproduce the 
- *     above copyright notice, this list of conditions and the 
- *     following disclaimer in the documentation and/or other 
- *     materials provided with the distribution.
- *   * Neither the name of X-ray Instrumentation Associates 
- *     nor the names of its contributors may be used to endorse 
- *     or promote products derived from this software without 
- *     specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
- * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
- * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
- * IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE 
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON 
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR 
- * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF 
- * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
- * SUCH DAMAGE.
- *
- * $Id: usblib.h,v 1.2 2007-10-23 02:07:16 rivers Exp $
- *
- */
-
-#ifndef _USBLIB_H_
-#define _USBLIB_H_
+/************/
+/*  usblib.h  */
+/************/
 
 #define CTRL_SIZE 5
+#ifdef LINUX
+#define IN2 2
+#define OUT1 1
+#define OUT2 2
+#define OUT4 4
+#else
 #define IN2 8
 #define OUT1 0
 #define OUT2 1
 #define OUT4 3
+#endif
 
 /* Few definitions shamelessly copied from ezusbsys.h provided by Cypress */
+#include "Dlldefs.h"
+#ifdef WIN32
+  #include <windows.h>
+#endif
+#ifdef LINUX
+  #include "xia_linux.h"
+#endif
 
 #define Ezusb_IOCTL_INDEX  0x0800
 
@@ -88,13 +64,10 @@ typedef struct _BULK_TRANSFER_CONTROL
 
 /* Function Prototypes */
 
-/*XIA_EXPORT int XIA_API usb_read(long address, long nWords, long ModNum, uint16* buffer);
-XIA_EXPORT int XIA_API usb_write(long address, long nWords, long ModNum, uint16* buffer);
+/*XIA_EXPORT int XIA_API xia_usb_read(long address, long nWords, long ModNum, uint16* buffer);
+XIA_EXPORT int XIA_API xia_usb_write(long address, long nWords, long ModNum, uint16* buffer);
 */
-XIA_EXPORT int XIA_API usb_close(HANDLE hDevice);
-XIA_EXPORT int XIA_API usb_open(char *device, HANDLE *hDevice);
-XIA_EXPORT int XIA_API usb_read(long address, long nWords, char *device, unsigned short *buffer);
-XIA_EXPORT int XIA_API usb_write(long address, long nWords, char *device, unsigned short *buffer);
-
-
-#endif /* _USBLIB_H_ */
+XIA_EXPORT int XIA_API xia_usb_close(HANDLE hDevice);
+XIA_EXPORT int XIA_API xia_usb_open(char *device, HANDLE *hDevice);
+XIA_EXPORT int XIA_API xia_usb_read(long address, long nWords, char *device, unsigned short *buffer);
+XIA_EXPORT int XIA_API xia_usb_write(long address, long nWords, char *device, unsigned short *buffer);
