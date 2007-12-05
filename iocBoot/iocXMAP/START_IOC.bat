2f6dcd85633cc8d0e6d3426ff99d6ec288ab2d89
Mark Rivers <MarkRivers@users.noreply.github.com>
2007-12-05 21:04:27 +0000

    Fixed error, should be dxpApp.exe, not xmapApp.exe

--- iocBoot/iocXMAP/START_IOC.bat
+++ iocBoot/iocXMAP/START_IOC.bat
@@ -4,5 +4,5 @@ REM set EPICS_DISPLAY_PATH=C:\epics_adls
 REM Note that we must use the Windows "start" command or medm won't find X11 dlls
 start medm -x -macro "P=dxpXMAP:, D=dxp, M=mca" 16element_dxp.adl
 REM Put Cygwin in the path so the EPICS application can find cygwin1.dll
-PATH=c:\cygwin\bin;C:\XIA_xmap_dlls
-..\..\bin\cygwin-x86\xmapApp.exe st.cmd
+PATH=c:\cygwin\bin
+..\..\bin\cygwin-x86\dxpApp.exe st.cmd
