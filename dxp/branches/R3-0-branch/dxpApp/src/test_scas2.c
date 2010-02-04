e626e084ab2c454f936d14b60ed8e9a76a5cb855
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2010-02-04 00:39:07 +0000

    Speeded up setting SCAs; changed from 16 to 12 channels

--- dxp/branches/R3-0-branch/dxpApp/src/test_scas2.c
+++ dxp/branches/R3-0-branch/dxpApp/src/test_scas2.c
@@ -3,7 +3,7 @@
 #include "handel_generic.h"
 
 #define NUM_SCAS 16
-#define NUM_CHANNELS 16
+#define NUM_CHANNELS 12
 #define NUM_MCA_BINS 2048
 #define NUM_LOOPS 5
 static char *sca_lo[NUM_SCAS];
@@ -24,7 +24,7 @@ int main(int argc, char **argv)
 
     printf("Initializing ...\n");
     xiaSetLogLevel(2);
-    xiaInit("xmap16.ini");
+    xiaInit("xmap12.ini");
     xiaStartSystem();
     for (i=0; i<NUM_SCAS; i++) {
         sca_lo[i] = calloc(1, SCA_NAME_LEN);
@@ -35,20 +35,30 @@ int main(int argc, char **argv)
     for (loop=0; loop<NUM_LOOPS; loop++) {
         printf("Loop = %d/%d\n", loop+1, NUM_LOOPS);
         for (i=0; i<NUM_CHANNELS; i++) {
+             printf("  channel=%d\n", i);
              xiaSetAcquisitionValues(i, "number_of_scas", &num_scas);
              for (j=0; j<NUM_SCAS; j++) {
-                 dvalue = 100.;
+                 printf("    setting SCA %d\n", j);
+                 dvalue = 100. * j;
                  status = xiaSetAcquisitionValues(i, sca_lo[j], &dvalue);
                  CHECK_STATUS(status);
-                 status = xiaBoardOperation(i, "apply", &ignore);
+                 /* status = xiaBoardOperation(i, "apply", &ignore);
+                 CHECK_STATUS(status); */
+                 dvalue = (100 * j) + 50;
+                 status = xiaSetAcquisitionValues(i, sca_hi[j], &dvalue);
                  CHECK_STATUS(status);
-                 dvalue = 150.;
-                 xiaSetAcquisitionValues(i, sca_hi[j], &dvalue);
-                 CHECK_STATUS(status);
-                 xiaBoardOperation(i, "apply", &ignore);
-                 CHECK_STATUS(status);
+                 /* xiaBoardOperation(i, "apply", &ignore);
+                 status = CHECK_STATUS(status); */
             }
-
+            /* Set the bin width in eV */
+            printf("    setting bin width\n");
+            dvalue = 20. * 1000. / NUM_MCA_BINS;
+            xiaStopRun(i);
+            xiaSetAcquisitionValues(i, "mca_bin_width", &dvalue);
+            printf("    calling apply\n");
+            status = xiaBoardOperation(i, "apply", &ignore);
+            CHECK_STATUS(status);
+            printf("    getting values\n");
             xiaGetParamData(i, "values", param_data);
             xiaGetRunData(i, "input_count_rate", &dvalue);
             xiaGetRunData(i, "output_count_rate", &dvalue);
@@ -68,12 +78,7 @@ int main(int argc, char **argv)
             xiaGetAcquisitionValues(i, "calibration_energy", &dvalue);
             xiaGetAcquisitionValues(i, "mca_bin_width", &dvalue);
             xiaGetAcquisitionValues(i, "number_mca_channels", &dvalue);
-            /* Set the bin width in eV */
-            dvalue = 20. * 1000. / NUM_MCA_BINS;
-            xiaStopRun(i);
-            xiaSetAcquisitionValues(i, "mca_bin_width", &dvalue);
-            status = xiaBoardOperation(i, "apply", &ignore);
-            CHECK_STATUS(status);
+            printf("    getting SCA data\n");
             xiaGetAcquisitionValues(i, "mca_bin_width", &dvalue);
             xiaGetAcquisitionValues(i, "number_of_scas", &dvalue);
             for (j=0; j<NUM_SCAS; j++) {
