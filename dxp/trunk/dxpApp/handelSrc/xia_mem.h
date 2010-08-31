8c98779918629f412d8ad35948ca1234da9f773b
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2010-08-31 21:07:56 +0000

    Handel 1.10 version

--- dxp/trunk/dxpApp/handelSrc/xia_mem.h
+++ dxp/trunk/dxpApp/handelSrc/xia_mem.h
@@ -1,7 +1,5 @@
 /*
- * xia_mem.h
- *
- * Copyright (c) 2005, XIA LLC
+ * Copyright (c) 2005-2009, XIA LLC
  *
  * All rights reserved.
  *
@@ -35,8 +33,8 @@
  * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
  * SUCH DAMAGE.
  *
+ * $Id$
  *
- * $Id: xia_mem.h,v 1.3 2009-07-06 18:24:32 rivers Exp $
  */
 
 #ifndef __XIA_MEM_H__
