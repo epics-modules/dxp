81284e2816ceec2b284b8fdd8122479d4532b2e1
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2010-08-31 21:18:27 +0000

    Handel 1.10 version

--- dxp/trunk/dxpApp/handelSrc/camac_errors.h
+++ dxp/trunk/dxpApp/handelSrc/camac_errors.h
@@ -32,7 +32,7 @@
  * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
  * SUCH DAMAGE.
  *
- * $Id: camac_errors.h,v 1.1 2009-07-06 18:24:28 rivers Exp $
+ * $Id: camac_errors.h 1135 2005-01-04 20:11:41Z SYSTEM $
  *
  */
 
