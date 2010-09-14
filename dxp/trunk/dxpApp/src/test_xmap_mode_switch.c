66827c2615974e25ba7de8619aa752dd9aed249a
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2010-09-14 17:00:15 +0000

    New test

---
+++ dxp/trunk/dxpApp/src/test_xmap_mode_switch.c
@@ -0,0 +1,39 @@
+/* This program tests switching between the mapping modes on the xMAP. 
+ * We are getting errors when downloading the firmware.
+ */
+
+#include <stdlib.h>
+#include "handel.h"
+#include "handel_generic.h"
+#include "handel_constants.h"
+
+#define MAX_ITERATIONS 100
+#define CHECK_STATUS(status) if (status) {printf("Error %d line %d\n", status, __LINE__); exit(status);}
+
+int main(int argc, char **argv)
+{
+    int iter, mode;
+    int status;
+    double dvalue;
+
+    printf("Initializing ...\n");
+    status = xiaSetLogLevel(2);
+    CHECK_STATUS(status);
+    status = xiaInit("xmap16.ini");
+    CHECK_STATUS(status);
+    status = xiaStartSystem();
+    CHECK_STATUS(status);
+
+    status = xiaSetLogLevel(4);
+    CHECK_STATUS(status);
+
+    for (iter=0; iter<MAX_ITERATIONS; iter++) {
+        for (mode=0; mode<3; mode++) {
+            printf("Iter=%d, setting mapping mode %d\n", iter, mode);
+            dvalue = mode;
+            status = xiaSetAcquisitionValues(-1, "mapping_mode", &dvalue);
+            CHECK_STATUS(status);
+        }
+    }
+    return(0);
+}
