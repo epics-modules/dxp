3d1719d4b3bb6fc880aa421493fb0db1d0c70c7b
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2010-09-20 10:01:33 +0000

    Report low-level parameters when there is a problem

--- dxp/trunk/dxpApp/src/test_xmap_mode_switch.c
+++ dxp/trunk/dxpApp/src/test_xmap_mode_switch.c
@@ -8,12 +8,16 @@
 #include "handel_constants.h"
 
 #define MAX_ITERATIONS 100
+static char *paramList[] = {"RUNERROR", "ERRINFO", "INITIALIZE", "MAPPINGMODE", "BOOTCOUNT", "GAINDAC"};
+#define MAX_PARAMS 6
 #define CHECK_STATUS(status) if (status) {printf("Error %d line %d\n", status, __LINE__); exit(status);}
 
 int main(int argc, char **argv)
 {
     int iter, mode, status, debugLevel=2;
+    int chan, param;
     double dvalue;
+    unsigned short paramValue;
 
     printf("Initializing ...\n");
     status = xiaSetLogLevel(debugLevel);
@@ -35,7 +39,16 @@ int main(int argc, char **argv)
             printf("Iter=%d, setting mapping mode %d\n", iter, mode);
             dvalue = mode;
             status = xiaSetAcquisitionValues(-1, "mapping_mode", &dvalue);
-            CHECK_STATUS(status);
+            if (status) {
+                for (chan=0; chan<16; chan++) {
+                    for (param=0; param<MAX_PARAMS; param++) {
+                        status = xiaGetParameter(chan, paramList[param], &paramValue);
+                        CHECK_STATUS(status);
+                        printf("mode=%d, chan=%d, param=%s, value=0x%x\n",
+                            mode, chan, paramList[param], paramValue);
+                    }
+                }
+            }
         }
     }
     return(0);
