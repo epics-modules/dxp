59e4742972ff1664baa5be723c9ac3a7278421f4
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2010-11-30 17:49:54 +0000

    Updates for R3-0

--- dxp/trunk/iocBoot/iocXMAP/START_IOC_Cygwin.bat
+++ dxp/trunk/iocBoot/iocXMAP/START_IOC_Cygwin.bat
@@ -1,8 +1,10 @@
 REM Tell medm where to look for .adl files.  Edit the following line, or comment it
 REM out if the environment variable is already set
-REM set EPICS_DISPLAY_PATH=C:\epics_adls
+set EPICS_DISPLAY_PATH=P:\epics\adl_devel
 REM Note that we must use the Windows "start" command or medm won't find X11 dlls
+REM Change this line to 4element_dxp.adl for a 4-element system, etc.
 start medm -x -macro "P=dxpXMAP:, D=dxp, M=mca" 16element_dxp.adl
 REM Put Cygwin in the path so the EPICS application can find cygwin1.dll
-PATH=c:\cygwin\bin
-..\..\bin\cygwin-x86\dxpApp.exe st.cmd
+REM Put areaDetector\bin\win32-x86 in the path so the file plugin DLLs can be found
+PATH=C:\cygwin\bin;J:\epics\devel\areaDetector\bin\cygwin-x86
+..\..\bin\cygwin-x86\dxpApp.exe 16element.cmd
