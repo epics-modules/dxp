5001222e634e6ac7713eef63e58691bb3e958857
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2010-12-03 18:25:26 +0000

    Changes for R3-0

--- dxp/trunk/iocBoot/iocDXP2X/4element.cmd
+++ dxp/trunk/iocBoot/iocDXP2X/4element.cmd
@@ -14,12 +14,19 @@ cd startup
 # Increase size of buffer for error logging from default 1256
 errlogInit(50000)
 
+# The default callback queue in EPICS base is only 2000 bytes. 
+# The dxp detector system needs this to be larger to avoid the error message: 
+# "callbackRequest: cbLow ring buffer full" 
+# right after the epicsThreadSleep at the end of this script
+callbackSetQueueSize(4000)
+
 # Tell EPICS all about the record types, device-support modules, drivers,
 # etc. in this build.
 dbLoadDatabase("$(TOP)/dbd/dxpVX.dbd")
 dxpVX_registerRecordDeviceDriver(pdbbase)
 
 putenv("EPICS_TS_MIN_WEST=300")
+putenv("EPICS_CA_MAX_ARRAY_BYTES=50000")
 
 # Setup for save_restore
 < ../save_restore_vxWorks.cmd
@@ -43,24 +50,22 @@ xiaSetLogLevel(2)
 xiaInit("dxp4c2x_4.ini")
 xiaStartSystem
 
-# DXPConfig(serverName, ndetectors, ngroups, pollFrequency)
-DXPConfig("DXP1",  4, 1, 100)
+# DXPConfig(serverName, ndetectors, maxBuffers, maxMemory)
+NDDxpConfig("DXP1",  4, -1, -1)
 
-dbLoadTemplate("4element.template")
-
-#xiaSetLogLevel(4)
-#asynSetTraceMask DXP1 0 255
-#asynSetTraceIOMask DXP1 0 2
-#var dxpRecordDebug 10
+dbLoadTemplate("4element.substitutions")
 
 ### Scan-support software
 # crate-resident scan.  This executes 1D, 2D, 3D, and 4D scans, and caches
 # 1D data, but it doesn't store anything to disk.  (See 'saveData' below for that.)
 dbLoadRecords("$(SSCAN)/sscanApp/Db/scan.db","P=dxp2X:,MAXPTS1=2000,MAXPTS2=1000,MAXPTS3=10,MAXPTS4=10,MAXPTSH=2048")
 
+# Free the memory allocated at the top
+free(mem)
+
 iocInit
 
-seq &dxpMED, "P=dxp2X:, DXP=dxp, MCA=mca, N_DETECTORS=4"
+seq &dxpMED, "P=dxp2X:, DXP=dxp, MCA=mca, N_DETECTORS=4, N_SCAS=16"
 
 ### Start up the autosave task and tell it what to do.
 # Save settings every thirty seconds
@@ -69,6 +74,4 @@ create_monitor_set("auto_settings4.req", 30, "P=dxp2X:")
 ### Start the saveData task.
 saveData_Init("saveData.req", "P=dxp2X:")
 
-# Free the memory allocated at the top
-free(mem)
 
