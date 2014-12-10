a1d457888ccee6a63ac59783806094e28b5e1f22
Mark Rivers <MarkRivers@users.noreply.github.com>
2014-12-10 17:13:48 +0000

    Fixed location of areaDetector template files

--- iocBoot/iocXMAP/16element.cmd
+++ iocBoot/iocXMAP/16element.cmd
@@ -30,21 +30,21 @@ dbLoadTemplate("16element.substitutions")
 
 # Create a netCDF file saving plugin
 NDFileNetCDFConfigure("DXP1NetCDF", 100, 0, "DXP1", 0)
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","P=dxpXMAP:,R=netCDF1:,PORT=DXP1NetCDF,ADDR=0,TIMEOUT=1,NDARRAY_PORT=DXP1,NDARRAY_ADDR=0")
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFile.template",      "P=dxpXMAP:,R=netCDF1:,PORT=DXP1NetCDF,ADDR=0,TIMEOUT=1")
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFileNetCDF.template","P=dxpXMAP:,R=netCDF1:,PORT=DXP1NetCDF,ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(ADCORE)/db/NDPluginBase.template","P=dxpXMAP:,R=netCDF1:,PORT=DXP1NetCDF,ADDR=0,TIMEOUT=1,NDARRAY_PORT=DXP1,NDARRAY_ADDR=0")
+dbLoadRecords("$(ADCORE)/db/NDFile.template",      "P=dxpXMAP:,R=netCDF1:,PORT=DXP1NetCDF,ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(ADCORE)/db/NDFileNetCDF.template","P=dxpXMAP:,R=netCDF1:,PORT=DXP1NetCDF,ADDR=0,TIMEOUT=1")
 
 # Create a TIFF file saving plugin
 NDFileTIFFConfigure("DXP1TIFF", 20, 0, "DXP1", 0)
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","P=dxpXMAP:,R=TIFF1:,PORT=DXP1TIFF,ADDR=0,TIMEOUT=1,NDARRAY_PORT=DXP1,NDARRAY_ADDR=0")
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFile.template",      "P=dxpXMAP:,R=TIFF1:,PORT=DXP1TIFF,ADDR=0,TIMEOUT=1")
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFileTIFF.template",  "P=dxpXMAP:,R=TIFF1:,PORT=DXP1TIFF,ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(ADCORE)/db/NDPluginBase.template","P=dxpXMAP:,R=TIFF1:,PORT=DXP1TIFF,ADDR=0,TIMEOUT=1,NDARRAY_PORT=DXP1,NDARRAY_ADDR=0")
+dbLoadRecords("$(ADCORE)/db/NDFile.template",      "P=dxpXMAP:,R=TIFF1:,PORT=DXP1TIFF,ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(ADCORE)/db/NDFileTIFF.template",  "P=dxpXMAP:,R=TIFF1:,PORT=DXP1TIFF,ADDR=0,TIMEOUT=1")
 
 # Create a NeXus file saving plugin
 NDFileNexusConfigure("DXP1Nexus", 20, 0, "DXP1", 0, 0, 80000)
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","P=dxpXMAP:,R=Nexus1:,PORT=DXP1Nexus,ADDR=0,TIMEOUT=1,NDARRAY_PORT=DXP1,NDARRAY_ADDR=0")
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFile.template",      "P=dxpXMAP:,R=Nexus1:,PORT=DXP1Nexus,ADDR=0,TIMEOUT=1")
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFileNexus.template", "P=dxpXMAP:,R=Nexus1:,PORT=DXP1Nexus,ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(ADCORE)/db/NDPluginBase.template","P=dxpXMAP:,R=Nexus1:,PORT=DXP1Nexus,ADDR=0,TIMEOUT=1,NDARRAY_PORT=DXP1,NDARRAY_ADDR=0")
+dbLoadRecords("$(ADCORE)/db/NDFile.template",      "P=dxpXMAP:,R=Nexus1:,PORT=DXP1Nexus,ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(ADCORE)/db/NDFileNexus.template", "P=dxpXMAP:,R=Nexus1:,PORT=DXP1Nexus,ADDR=0,TIMEOUT=1")
 
 
 #xiaSetLogLevel(4)
