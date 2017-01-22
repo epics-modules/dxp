3968e403c784522c6db52926b0103daaa7e987c3
Mark Rivers <rivers@cars.uchicago.edu>
2017-01-22 14:31:20 -0600

    Changed ARCH from win32-x86 to win32-x86-static

--- iocBoot/iocSaturn/START_IOC_WIN32.bat
+++ iocBoot/iocSaturn/START_IOC_WIN32.bat
@@ -4,8 +4,8 @@ set EPICS_DISPLAY_PATH=P:\epics\adl_devel
 REM Note that we must use the Windows "start" command or medm won't find X11 dlls
 start medm -x -macro "P=dxpSaturn:, D=dxp1:, M=mca1"  dxpSaturn.adl
 REM Put areaDetector\bin\win32-x86 in the path so the file plugin DLLs can be found
-PATH=J:\epics\devel\areaDetector\bin\win32-x86
-..\..\bin\win32-x86\dxpApp.exe st.cmd
+PATH=J:\epics\devel\areaDetector\bin\win32-x86-static
+..\..\bin\win32-x86-static\dxpApp.exe st.cmd
 REM Put a pause so user can see any error messages when IOC closes
 pause
 
