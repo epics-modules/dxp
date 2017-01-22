cb0941b6e36c45295d068c7f2e5f56179ed728ff
Mark Rivers <rivers@cars.uchicago.edu>
2017-01-22 14:39:27 -0600

    Use PREFIX macro; add devIocStats

--- iocBoot/iocMercury/4element.cmd
+++ iocBoot/iocMercury/4element.cmd
@@ -8,6 +8,9 @@ dxp_registerRecordDeviceDriver(pdbbase)
 # The search path for database files
 epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(ADCORE)/db")
 
+# The detector prefix
+epicsEnvSet("PREFIX", "dxpMercury:")
+
 # The default callback queue in EPICS base is only 2000 bytes. 
 # The dxp detector system needs this to be larger to avoid the error message: 
 # "callbackRequest: cbLow ring buffer full" 
@@ -16,8 +19,8 @@ callbackSetQueueSize(4000)
 
 # Setup for save_restore
 < ../save_restore.cmd
-save_restoreSet_status_prefix("dxpMercury:")
-dbLoadRecords("$(AUTOSAVE)/asApp/Db/save_restoreStatus.db", "P=dxpMercury:")
+save_restoreSet_status_prefix("$(PREFIX)")
+dbLoadRecords("$(AUTOSAVE)/asApp/Db/save_restoreStatus.db", "P=$(PREFIX)")
 set_pass0_restoreFile("auto_settings4.sav")
 set_pass1_restoreFile("auto_settings4.sav")
 
@@ -33,15 +36,15 @@ dbLoadTemplate("4element.substitutions")
 
 # Create a netCDF file saving plugin
 NDFileNetCDFConfigure("DXP1NetCDF", 100, 0, DXP1, 0)
-dbLoadRecords("$(ADCORE)/db/NDFileNetCDF.template","P=dxpMercury:,R=netCDF1:,PORT=DXP1NetCDF,ADDR=0,TIMEOUT=1,NDARRAY_PORT=DXP1")
+dbLoadRecords("$(ADCORE)/db/NDFileNetCDF.template","P=$(PREFIX),R=netCDF1:,PORT=DXP1NetCDF,ADDR=0,TIMEOUT=1,NDARRAY_PORT=DXP1")
 
 # Create a TIFF file saving plugin
 NDFileTIFFConfigure("DXP1TIFF", 20, 0, "DXP1", 0)
-dbLoadRecords("$(ADCORE)/db/NDFileTIFF.template",  "P=dxpMercury:,R=TIFF1:,PORT=DXP1TIFF,ADDR=0,TIMEOUT=1,NDARRAY_PORT=DXP1")
+dbLoadRecords("$(ADCORE)/db/NDFileTIFF.template",  "P=$(PREFIX),R=TIFF1:,PORT=DXP1TIFF,ADDR=0,TIMEOUT=1,NDARRAY_PORT=DXP1")
 
 # Create a NeXus file saving plugin
 NDFileNexusConfigure("DXP1Nexus", 20, 0, "DXP1", 0, 0, 80000)
-dbLoadRecords("$(ADCORE)/db/NDFileNexus.template", "P=dxpMercury:,R=Nexus1:,PORT=DXP1Nexus,ADDR=0,TIMEOUT=1,NDARRAY_PORT=DXP1")
+dbLoadRecords("$(ADCORE)/db/NDFileNexus.template", "P=$(PREFIX),R=Nexus1:,PORT=DXP1Nexus,ADDR=0,TIMEOUT=1,NDARRAY_PORT=DXP1")
 
 #xiaSetLogLevel(4)
 #asynSetTraceMask DXP1 0 0x11
@@ -50,25 +53,28 @@ dbLoadRecords("$(ADCORE)/db/NDFileNexus.template", "P=dxpMercury:,R=Nexus1:,PORT
 ### Scan-support software
 # crate-resident scan.  This executes 1D, 2D, 3D, and 4D scans, and caches
 # 1D data, but it doesn't store anything to disk.  (See 'saveData' below for that.)
-dbLoadRecords("$(SSCAN)/sscanApp/Db/scan.db","P=dxpMercury:,MAXPTS1=2000,MAXPTS2=1000,MAXPTS3=10,MAXPTS4=10,MAXPTSH=2048")
+dbLoadRecords("$(SSCAN)/sscanApp/Db/scan.db","P=$(PREFIX),MAXPTS1=2000,MAXPTS2=1000,MAXPTS3=10,MAXPTS4=10,MAXPTSH=2048")
+
+# Load devIocStats
+dbLoadRecords("$(DEVIOCSTATS)/db/iocAdminSoft.db", "IOC=$(PREFIX)")
 
 iocInit
 
-seq dxpMED, "P=dxpMercury:, DXP=dxp, MCA=mca, N_DETECTORS=4, N_SCAS=32"
+seq dxpMED, "P=$(PREFIX), DXP=dxp, MCA=mca, N_DETECTORS=4, N_SCAS=32"
 
 ### Start up the autosave task and tell it what to do.
 # Save settings every thirty seconds
-create_monitor_set("auto_settings4.req", 30, "P=dxpMercury:")
+create_monitor_set("auto_settings4.req", 30, "P=$(PREFIX)")
 
 ### Start the saveData task.
-saveData_Init("saveData.req", "P=dxpMercury:")
+saveData_Init("saveData.req", "P=$(PREFIX)")
 
 # Sleep for 15 seconds to let initialization complete and then turn on AutoApply and do Apply manually once
 epicsThreadSleep(15.)
 # Turn on AutoApply
-dbpf("dxpMercury:AutoApply", "Yes")
+dbpf("$(PREFIX)AutoApply", "Yes")
 # Manually do Apply once
-dbpf("dxpMercury:Apply", "1")
+dbpf("$(PREFIX)Apply", "1")
 # Seems to be necessary to resend AutoPixelsPerBuffer to read back correctly from Handel
-dbpf("dxpMercury:AutoPixelsPerBuffer.PROC", "1")
+dbpf("$(PREFIX)AutoPixelsPerBuffer.PROC", "1")
 
